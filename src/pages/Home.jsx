ef7a1f1c2f34c727c73c87436aca7c81082ca814
KaBarbi <kabarbizz@gmail.com>
2025-02-15 22:35:34 -0300

    [FIX]: build vercel error

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import React from "react"
-import Card from "../components/card"
-import Contact from "../components/Contact"
+import Card from "../components/Card/index.jsx"
+import Contact from "../components/Contact/index.jsx"
 
 const Home = () => {
     return (
