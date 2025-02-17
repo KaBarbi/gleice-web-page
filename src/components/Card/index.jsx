c5d535372fb07e8260d4eb1a63b78bbc0b7c3bf3
KaBarbi <kabarbizz@gmail.com>
2025-02-17 12:06:47 -0300

    [FEAT]: button add hover

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -4,32 +4,36 @@ import { Link } from "react-router-dom"
 const Card = () => {
     return (
         <div className="card-wrapper">
-        <div className="container">
-            <div className="card-img">
-                <img src="/images/gleice.jpg" alt="Descrição" />
-            </div>
-            <div className="card-content">
-                <h2 className="card-title">Gleice Discord Bot</h2>
-                <p className="card-description">
-                    Personal project created to explore and expand my
-                    programming knowledge. Through it, I am learning to develop
-                    and integrate various technologies.
-                </p>
-                <div className="button-container">
-                    <a
-                        href="https://discord.com/oauth2/authorize?client_id=1306407860493877258&permissions=8&integration_type=0&scope=bot"
-                        className="custom-btn"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        Add
-                    </a>
-                    <Link to="/commands" className="custom-btn secondary-btn">
-                        Commands
-                    </Link>
+            <div className="container">
+                <div className="card-img">
+                    <img src="/images/gleice.jpg" alt="Descrição" />
+                </div>
+                <div className="card-content">
+                    <h2 className="card-title">Gleice Discord Bot</h2>
+                    <p className="card-description">
+                        Personal project created to explore and expand my
+                        programming knowledge. Through it, I am learning to
+                        develop and integrate various technologies.
+                    </p>
+                    <div className="button-container">
+                        <a
+                            href="https://discord.com/oauth2/authorize?client_id=1306407860493877258&permissions=8&integration_type=0&scope=bot"
+                            className="custom-btn"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            <span>Add</span>
+                        </a>
+
+                        <Link
+                            to="/commands"
+                            className="custom-btn secondary-btn"
+                        >
+                            Commands
+                        </Link>
+                    </div>
                 </div>
             </div>
-        </div>
         </div>
     )
 }
